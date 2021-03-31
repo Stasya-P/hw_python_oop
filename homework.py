17752379e9126f6f29ff7d6d49c11719dcbb09a2
Anastasia Piatova <anapiatova97@gmail.com>
2021-03-31 10:49:19 +0300

    Sprint 2 project, second review

--- homework.py
+++ homework.py
@@ -84,22 +84,3 @@ class CashCalculator(Calculator):
         else:
             return('Денег нет, держись: твой долг '
                    f'- {rate_all} {currency_name}')
-
-
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
-
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='08.11.2019'))
-
-print(cash_calculator.get_today_cash_remained('rub'))
-# должно напечататься
-# На сегодня осталось 555 руб
